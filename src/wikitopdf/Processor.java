9548a342c406fd7dfbfeab69d1c1268a3e2b0850
mandiberg-studio <ram@ram-PC>
2013-05-11 21:56:37 -0400

    Changes to not write PDFImage

--- src/wikitopdf/Processor.java
+++ src/wikitopdf/Processor.java
@@ -4,8 +4,10 @@
 package wikitopdf;
 
 import java.util.ArrayList;
+
 import wikitopdf.pdf.Pdf2ImageConverter;
-import wikitopdf.pdf.PdfHDPageWrapper;
+import wikitopdf.pdf.PdfPageWrapper;
+import wikitopdf.pdf.PdfStamp;
 import wikitopdf.utils.ByteFormatter;
 import wikitopdf.utils.WikiLogger;
 import wikitopdf.utils.WikiSettings;
@@ -32,43 +34,51 @@ public class Processor implements Runnable {
     public void run() {
         try {
 
-            PdfHDPageWrapper pdfWrapper = new PdfHDPageWrapper(currentPage);
+           // PdfHDPageWrapper pdfWrapper = new PdfHDPageWrapper(currentPage);
+            PdfPageWrapper pdfWrapper = new PdfPageWrapper(currentPage);
 
                 //while (pdfWrapper.getPageNumb() < pdfPageLimit && isInProggress) {
 
-                    ArrayList<WikiPage> pages = sqlReader.getBunch(currentPage, pageBunch, 1);
-                    for (WikiPage page : pages) {
-                        pdfWrapper.writePage(page);
-                    }
+            ArrayList<WikiPage> pages = sqlReader.getBunch(currentPage, pageBunch, 1);
+            for (WikiPage page : pages) {
+            	pdfWrapper.writePage(page);
+            }
 
                     //cPageNum +=
 
-                    isInProggress = sqlReader.isInProggres();
-                    //TODO change from test value
-                    //isInProggress = false;
-                    if (isInProggress) {
-                        currentPage += pageBunch;
-                    }
+            isInProggress = sqlReader.isInProggres();
+            //TODO change from test value
+            //isInProggress = false;
+            if (isInProggress) {
+            	currentPage += pageBunch;
+            }
 
-                    //System.out.println(pdfWrapper.getPageNumb());
-                    //pdfWrapper.closeColumn();
-                //}
+            //System.out.println(pdfWrapper.getPageNumb());
+            //pdfWrapper.closeColumn();
+            //}
+            
+            //cPageNum = pdfWrapper.getPageNumb();
+            pdfWrapper.close();
 
-                //cPageNum = pdfWrapper.getPageNumb();
-                pdfWrapper.close();
-
-                //Convert pdf to images
-                Pdf2ImageConverter converter = new Pdf2ImageConverter();
-                converter.convert(pdfWrapper.getOutputFileName());
+            //Convert pdf to images
+            //Not need for now 05/11/2013
+//            Pdf2ImageConverter converter = new Pdf2ImageConverter();
+//                converter.convert(pdfWrapper.getOutputFileName());
 
                 //Info
-                pageInfo = "([" + pdfWrapper.getCurrentArticleID() + "] " +
-                        pdfWrapper.getCurrentTitle() + ")";
-                WikiLogger.getLogger().fine(Thread.currentThread().getName() + " Retrieved " + startPage + "-" + currentPage  + " articles " + pageInfo);
-                WikiLogger.getLogger().info("Free memory: " + ByteFormatter.format(runtime.freeMemory()));
-
+            pageInfo = "([" + pdfWrapper.getCurrentArticleID() + "] " +
+            		pdfWrapper.getCurrentTitle() + ")";
+            WikiLogger.getLogger().fine(Thread.currentThread().getName() + " Retrieved " + startPage + "-" + currentPage  + " articles " + pageInfo);
+            WikiLogger.getLogger().info("Free memory: " + ByteFormatter.format(runtime.freeMemory()));
+			// Stamping all page numbers
+            PdfStamp stamp = new PdfStamp();
+			WikiLogger.getLogger().info("Stamping page numbers...");
+			stamp.stampDir(pdfWrapper.getPageNumb());
+			WikiLogger.getLogger().fine("Stamping done");
         } catch (Exception ex) {
             ex.printStackTrace();
+        }finally {
+        	sqlReader.close();
         }
     }
 
