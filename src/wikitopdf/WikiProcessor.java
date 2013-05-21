038af17fd27bb45eb04ed23cf16cead11c3a71fc
ColinDHElliott <Colin_Elliot@banffcentre.ca>
2013-05-21 14:53:26 -0600

    Documentation for WikiProcessor

--- src/wikitopdf/WikiProcessor.java
+++ src/wikitopdf/WikiProcessor.java
@@ -39,33 +39,35 @@ public class WikiProcessor {
         try {
 
             sqlReader = new SQLProcessor();
-            int artCount = sqlReader.getArticlesCount();
+            int artCount = sqlReader.getArticlesCount();// Counts total from database
 
 
             while (isInProggress) {
 
-                pdfWrapper = new PdfPageWrapper(startLimit);
-
-                while (pdfWrapper.getPageNumb() < pdfPageLimit && isInProggress) {
+                pdfWrapper = new PdfPageWrapper(startLimit); // Start with page ID indicated in settings
+                
+                // While still pages in database and still writing pages to this volume 
+                // This inner while loop creates a single pdf volume
+                while (pdfWrapper.getPageNumb() < pdfPageLimit && isInProggress) { 
                     
-                    ArrayList<WikiPage> pages = sqlReader.getBunch(startLimit, pageBunch, 1);
+                    // Get all article entries from the database
+                    ArrayList<WikiPage> pages = sqlReader.getBunch(startLimit, pageBunch, 1); 
                     for (WikiPage page : pages) {
                         pdfWrapper.writePage(page);
                     }
 
                     //cPageNum +=
 
-                    isInProggress = sqlReader.isInProggres();
+                    isInProggress = sqlReader.isInProggres(); // checks to see if there is still database entries
                     //TODO change from test value
                     //isInProggress = false;
                     if (isInProggress) {
-                        startLimit += pageBunch;
-
+                        startLimit += pageBunch; //auto-incraments start based on a database query bunch  
                     }
 
                     System.out.println("sarting PDF, page number" + pdfWrapper.getPageNumb()); // the number that is being printed
                     //pdfWrapper.closeColumn();
-                }
+                } // End of Volume
 
                 cPageNum = pdfWrapper.getPageNumb();
                 pdfWrapper.close();
@@ -76,7 +78,7 @@ public class WikiProcessor {
                         pdfWrapper.getCurrentTitle() + ")";
                 WikiLogger.getLogger().fine("Retrieved " + startLimit + "/" + artCount + " articles " + pageInfo);
                 WikiLogger.getLogger().info("Free memory: " + ByteFormatter.format(runtime.freeMemory()));
-            }
+            } //End of all volumes/PDFs
 
             //Stamping all page numbers
             PdfStamp stamp = new PdfStamp();
