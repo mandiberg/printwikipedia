59a8f8cdc234eb87cf83b510657faf46ab46b073
Kenny Lozowski <kenny_lozowski@banffcentre.ca>
2013-05-24 11:52:37 -0600

    Switch Volume Size from Articles to Pages

--- src/wikitopdf/WikiProcessor.java
+++ src/wikitopdf/WikiProcessor.java
@@ -7,8 +7,7 @@ import com.lowagie.text.DocumentException;
 import java.io.IOException;
 import java.io.File;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.Date;
+import java.util.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import wikitopdf.pdf.PdfPageWrapper;
@@ -31,13 +30,15 @@ public class WikiProcessor {
         int startLimit = WikiSettings.getInstance().getStartPage();
         boolean isInProggress = true;
         int cPageNum = 0;
+        int artWritten = 0;
+        int numArt = 0;
         int cVolNum = 1;
         Date newTime = new Date();
         Date oldTime;
-        //String tempName = "";
-        //String outputName = "";
-        //File oldName;
-        //File newName;
+        String tempName;
+        String outputName = "";
+        File oldFile;
+        File newFile;
 
         SQLProcessor sqlReader = null;
         PdfPageWrapper pdfWrapper = null;
@@ -54,7 +55,7 @@ public class WikiProcessor {
             while (isInProggress) {
 
                 pdfWrapper = new PdfPageWrapper(startLimit); // Start with page ID indicated in settings
-                //tempName = pdfWrapper.getOutputFileName(); // Added Wednesday May 22 by CE For file rename
+                tempName = "output\\" + pdfWrapper.getOutputFileName(); // Added Wednesday May 22 by CE For file rename
                 
                 // While still pages in database and still writing pages to this volume 
                 // This inner while loop creates a single pdf volume
@@ -62,37 +63,57 @@ public class WikiProcessor {
                     
                     // Get all article entries from the database
                     ArrayList<WikiPage> pages = sqlReader.getBunch(startLimit, pageBunch, 1); 
-                    for (WikiPage page : pages) {
+                    //for (WikiPage page : pages) {
+                      //  pdfWrapper.writePage(page);
+                    //}
+                    
+                    // Added May 23 by CE to stop writing articles when page limit is reached
+                    outputName = pages.get(0).getTitle();
+                    Iterator<WikiPage> i = pages.iterator();
+                    for(; i.hasNext() && pdfWrapper.getPageNumb() < pdfPageLimit; ) { 
+                        WikiPage page = i.next();
                         pdfWrapper.writePage(page);
+                        artWritten++;
                     }
-
-                    //cPageNum +=
+                    outputName = outputName + "-" + pages.get(artWritten - 1).getTitle();
 
                     isInProggress = sqlReader.isInProggres(); // checks to see if there is still database entries
                     //TODO change from test value
                     //isInProggress = false;
                     if (isInProggress) {
-                        startLimit += pageBunch; //auto-incraments start based on a database query bunch  
+                        //startLimit += pageBunch; //auto-incraments start based on a database query bunch
+                        startLimit += artWritten; // Added May 23 by CE incraments by the number of articles written
                     }
-
-                    System.out.println("sarting PDF, page number" + pdfWrapper.getPageNumb()); // the number that is being printed
+                    
+                    numArt = artWritten;
+                    artWritten = 0; // Added May 23 by CE resets incramentor so that it doesn't skip articles next loop through
+                    
+                    //System.out.println("starting PDF, page number: " + pdfWrapper.getPageNumb()); // the number that is being printed
                     //pdfWrapper.closeColumn();
                 } // End of Volume
-
+                
                 cPageNum = pdfWrapper.getPageNumb();
                 pdfWrapper.close();
                 
-                //Renaming Added Wendesday May 22 by CE
-                //oldName = new File(tempName);
-                //newName = new File(outputName);
+                //Renaming Added May 24 by CE, renames outputfile
+                outputName = "output\\Vol_" + cVolNum + "-" + outputName + "-" + (cPageNum - 1) + ".pdf";
+                oldFile = new File(tempName);
+                newFile = new File(outputName);
+                if(newFile.exists()){
+                    newFile.delete();
+                }
+                if(!(oldFile.renameTo(newFile))){
+                    System.out.println("File not renamed");
+                }
                 
-                //Time
+                //Timing
                 oldTime = newTime;
                 newTime = new Date();
-                //Print time for last volume in seconds
-                System.out.println("Time for vol" + cVolNum + ": " + (newTime.getTime() - oldTime.getTime())/1000);
+                //Print time for last volume in seconds, as well as the number of articles used to write it.
+                System.out.println("Time for Vol_" + cVolNum + ": " + ((newTime.getTime() - oldTime.getTime()) / 1000)
+                        + ", articles written: " + numArt);
                 //Print out current time
-                System.out.println("Current Time: " + newTime.getTime());
+                //System.out.println("Current Time: " + newTime.getTime());
                 cVolNum++;
 
                 //Info
