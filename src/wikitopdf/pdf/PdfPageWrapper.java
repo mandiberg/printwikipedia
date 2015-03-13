aea6f8236a9320e630336aa65bfca4e72b6912ff
System Administrator <root@wikis-mac-mini.home>
2015-03-13 12:52:29 -0400

    prevent wiktionary redirect from appearing. no template function to handle this

--- src/wikitopdf/pdf/PdfPageWrapper.java
+++ src/wikitopdf/pdf/PdfPageWrapper.java
@@ -187,6 +187,119 @@ public class PdfPageWrapper {
         tfs.addFont(oriya);
         tfs.addFont(fser);
     }
+    public void preFontGet(){
+        String path_to_fonts = "/Users/wiki/repos/printwikipedia/dist/fonts/";
+        FontFactory.register(path_to_fonts+"Cardo104s.ttf","cardo");
+        FontFactory.register(path_to_fonts+"msgothic.tcc.0","fontGlyph");
+        FontFactory.register(path_to_fonts+"cwTeXFangSong-zhonly.ttf","chinese1");
+        FontFactory.register(path_to_fonts+"cwTeXHei-zhonly.ttf","chinese2");
+        FontFactory.register(path_to_fonts+"cwTeXKai-zhonly.tff","chinese3");
+        FontFactory.register(path_to_fonts+"cwTeXMing-zhonly.ttf","chinese4");
+        FontFactory.register(path_to_fonts+"cwTeXYen-zhonly.ttf","chinese5");
+        FontFactory.register(path_to_fonts+"G5LISL1B.TTF","chinese6");
+        FontFactory.register(path_to_fonts+"Amiri-Regular.ttf","arab1");
+        FontFactory.register(path_to_fonts+"DroidKufi-Regular.ttf","arab2");
+        FontFactory.register(path_to_fonts+"Alef-Regular.ttf","hebrew");
+        FontFactory.register(path_to_fonts+"NotoSansCherokee-Regular.ttf","cherokee");
+        FontFactory.register(path_to_fonts+"NotoSansGeorgian-Regular.ttf","georgian");
+        FontFactory.register(path_to_fonts+"NotoSansDevanagari-Regular.ttf","devanagari");
+        FontFactory.register(path_to_fonts+"NanumGothic-Regular.ttf","nanum");
+        FontFactory.register(path_to_fonts+"NotoKufiArabic-Regular.ttf","arab3");
+        FontFactory.register(path_to_fonts+"NotoSansJP-Regular.otf","jap");
+        FontFactory.register(path_to_fonts+"NotoSansKhmer-Regular.ttf","khmer");
+        FontFactory.register(path_to_fonts+"NotoSansThai-Regular.ttf","thai");
+        FontFactory.register(path_to_fonts+"NotoSerifArmenian-Regular.ttf","armenian");
+        FontFactory.register(path_to_fonts+"NotoSansTamilUI-Regular.ttf","tamil");
+        FontFactory.register(path_to_fonts+"DejaVuSans.ttf","dvs");
+        FontFactory.register(path_to_fonts+"Roboto-Regular.ttf","roboto");
+        FontFactory.register(path_to_fonts+"OpenSans-Light.ttf","ops");
+        FontFactory.register(path_to_fonts+"MCTIME.TTF","russ");
+        FontFactory.register(path_to_fonts+"FreeSerif.ttf","fser");
+        FontFactory.register(path_to_fonts+"NotoSansSinhala-Regular.ttf","sinhala");
+        FontFactory.register(path_to_fonts+"NotoSansBengali-Regular.ttf","bengali");
+        FontFactory.register(path_to_fonts+"lohit_gu.ttf","punj");
+        FontFactory.register(path_to_fonts+"FreeSans.ttf","fsans");
+        FontFactory.register(path_to_fonts+"NotoSansTelugu-Regular.ttf","telugu");
+        FontFactory.register(path_to_fonts+"Cybercjk.ttf","cjk");
+        FontFactory.register(path_to_fonts+"IndUni-N-Roman.otf","ind");
+        FontFactory.register(path_to_fonts+"lohit_or.ttf","oriya");
+        System.out.println(FontFactory.getRegisteredFonts().toString());
+
+
+
+        int font_size = 6;
+        Font cardo = FontFactory.getFont("cardo", BaseFont.IDENTITY_H, BaseFont.EMBEDDED, font_size);
+        Font roboto = FontFactory.getFont("roboto", BaseFont.IDENTITY_H, BaseFont.EMBEDDED, font_size);
+        Font russ = FontFactory.getFont("russ", BaseFont.IDENTITY_H, BaseFont.EMBEDDED, font_size);
+        Font chinese1 = FontFactory.getFont("chinese1", BaseFont.IDENTITY_H, BaseFont.EMBEDDED, font_size);
+        Font chinese2 = FontFactory.getFont("chinese2", BaseFont.IDENTITY_H, BaseFont.EMBEDDED, font_size);
+        Font chinese3 = FontFactory.getFont("chinese3", BaseFont.IDENTITY_H, BaseFont.EMBEDDED, font_size);
+        Font chinese4 = FontFactory.getFont("chinese4", BaseFont.IDENTITY_H, BaseFont.EMBEDDED, font_size);
+        Font chinese5 = FontFactory.getFont("chinese5", BaseFont.IDENTITY_H, BaseFont.EMBEDDED, font_size);
+        Font chinese6 = FontFactory.getFont("chinese6", BaseFont.IDENTITY_H, BaseFont.EMBEDDED, font_size);
+        Font arab1 = FontFactory.getFont("arab1", BaseFont.IDENTITY_H, BaseFont.EMBEDDED, font_size);
+        Font arab2 = FontFactory.getFont("arab2", BaseFont.IDENTITY_H, BaseFont.EMBEDDED, font_size);
+        Font arab3 = FontFactory.getFont("arab3", BaseFont.IDENTITY_H, BaseFont.EMBEDDED, font_size);
+        Font hebrew = FontFactory.getFont("hebrew", BaseFont.IDENTITY_H, BaseFont.EMBEDDED, font_size);
+        Font cherokee = FontFactory.getFont("cherokee", BaseFont.IDENTITY_H, BaseFont.EMBEDDED, font_size);
+        Font georgian = FontFactory.getFont("georgian", BaseFont.IDENTITY_H, BaseFont.EMBEDDED, font_size);
+        Font devanagari = FontFactory.getFont("devanagari", BaseFont.IDENTITY_H, BaseFont.EMBEDDED, font_size);
+        Font nanum = FontFactory.getFont("nanum", BaseFont.IDENTITY_H, BaseFont.EMBEDDED, font_size);
+        Font jap = FontFactory.getFont("jap", BaseFont.IDENTITY_H, BaseFont.EMBEDDED, font_size);
+        Font khmer = FontFactory.getFont("khmer", BaseFont.IDENTITY_H, BaseFont.EMBEDDED, font_size);
+        Font thai = FontFactory.getFont("thai", BaseFont.IDENTITY_H, BaseFont.EMBEDDED, font_size);
+        Font tamil = FontFactory.getFont("tamil", BaseFont.IDENTITY_H, BaseFont.EMBEDDED, font_size);
+        Font armenian = FontFactory.getFont("armenian", BaseFont.IDENTITY_H, BaseFont.EMBEDDED, font_size);
+        Font sinhala = FontFactory.getFont("sinhala", BaseFont.IDENTITY_H, BaseFont.EMBEDDED, font_size);
+        Font ops = FontFactory.getFont("ops", BaseFont.IDENTITY_H, BaseFont.EMBEDDED, font_size);
+        Font bengali = FontFactory.getFont("bengali", BaseFont.IDENTITY_H, BaseFont.EMBEDDED, font_size);
+        Font punj = FontFactory.getFont("punj", BaseFont.IDENTITY_H, BaseFont.EMBEDDED, font_size);
+        Font fsans = FontFactory.getFont("fsans", BaseFont.IDENTITY_H, BaseFont.EMBEDDED, font_size);
+        Font dvs = FontFactory.getFont("dvs", BaseFont.IDENTITY_H, BaseFont.EMBEDDED, font_size);                    
+        Font telugu = FontFactory.getFont("telugu", BaseFont.IDENTITY_H, BaseFont.EMBEDDED, font_size);
+        Font cjk = FontFactory.getFont("cjk", BaseFont.IDENTITY_H, BaseFont.EMBEDDED, font_size);
+        Font ind = FontFactory.getFont("ind", BaseFont.IDENTITY_H, BaseFont.EMBEDDED, font_size);                    
+        Font oriya = FontFactory.getFont("oriya", BaseFont.IDENTITY_H, BaseFont.EMBEDDED, font_size);
+        Font fser = FontFactory.getFont("fser", BaseFont.IDENTITY_H, BaseFont.EMBEDDED, font_size);
+        Font fontGlyph = FontFactory.getFont("fontGlyph", BaseFont.IDENTITY_H, BaseFont.EMBEDDED, font_size);
+
+        pfs.addFont(cardo);
+        pfs.addFont(fontGlyph);
+        //fs.addFont(dvs);
+        pfs.addFont(fser);
+        pfs.addFont(cjk);
+        pfs.addFont(russ);
+        pfs.addFont(armenian);
+        pfs.addFont(chinese1);
+        pfs.addFont(chinese2);
+        pfs.addFont(chinese3);
+        pfs.addFont(chinese4);
+        pfs.addFont(chinese5);
+        pfs.addFont(chinese6);
+        pfs.addFont(arab1);
+        pfs.addFont(arab2);
+        pfs.addFont(arab3);
+//                    fs.addFont(ind);
+        pfs.addFont(hebrew);
+        pfs.addFont(cherokee);
+        pfs.addFont(georgian);  
+        pfs.addFont(devanagari);
+        pfs.addFont(nanum);
+        pfs.addFont(jap);
+        pfs.addFont(khmer);
+        pfs.addFont(thai);
+        pfs.addFont(tamil);
+        pfs.addFont(ops);
+//        fs.addFont(helv);
+        pfs.addFont(roboto);
+        pfs.addFont(sinhala);
+        pfs.addFont(bengali);
+        pfs.addFont(punj);
+        pfs.addFont(fsans);
+        pfs.addFont(telugu);
+        pfs.addFont(oriya);
+        pfs.addFont(fser);
+    }
             public void fontGet() throws DocumentException, IOException{
         String path_to_fonts = "/Users/wiki/repos/printwikipedia/dist/fonts/";
         FontFactory.register(path_to_fonts+"Cardo104s.ttf","cardo");
@@ -308,6 +421,9 @@ public class PdfPageWrapper {
     public void writePage(WikiPage page) {
         currentTitle = page.getTitle();
         currentArticleID = page.getId();
+        String x = page.getRevision().getText();
+        if(x.contains("wiktionary redirect"))
+            return;
         //System.out.println("Article ID is" + currentArticleID);
         writeTitle(currentTitle);
         writeText(page.getRevision().getText());
@@ -318,6 +434,7 @@ public class PdfPageWrapper {
     //Double paragraph helvetica problem is here other is in WikiHtmlConverter.java
     private void writeTitle(String line) {
         Phrase ph;
+        
         try {
             line = line.replaceAll("_", " ").toUpperCase();
             header.setCurrentTitle(line);
@@ -338,7 +455,8 @@ public class PdfPageWrapper {
 
             mct.addElement(pr);
             pdfDocument.add(mct);
-        } catch (Exception ex) {
+        } 
+        catch (Exception ex) {
             WikiLogger.getLogger().severe(currentTitle + " - Error: " + ex.getMessage());
         }
     }
@@ -346,25 +464,22 @@ public class PdfPageWrapper {
     //Write article text using defined styles
     private void writeText(String text) {
         try {
-            
-//            if(text.toLowerCase().contains("{{Infobox".toLowerCase())){//check if there's an infobox
-//                System.out.println(text + " text test"); 
-//                if(text.toLowerCase().contains("<ref".toLowerCase())){//if there is a reference inside the infobox
-//                    text.replaceAll("(<ref).+?(?:(</ref>)", "");//take care of references first.
-//                }
-//            }
-                    
+//            text = text.replaceAll("(\\{\\{Orphan\\|date).+(\\}\\})","");
+//            text = text.replaceAll("(official_name\\s+\\=).+?(\\|)","");
+//            text = text.replaceAll("(name\\s+\\=).+?(\\|)","");
+            System.out.println("77777" + text );
             // text is in BBCode (This is bliki)
             String html = WikiHtmlConverter.convertToHtml(text);
-            System.out.println(html + " $$$$");
+            System.out.println(html);
             //<a id="References" name="References"></a><H2>REFERENCES</H2>
             html = html.replaceAll("(?s)(<a id=\"See_also\" name=\"See_also\"></a><H2>SEE ALSO</H2>).*", "<b>_____________________</b><br /><br />");
             html = html.replaceAll("(?s)(<a id=\"References\" name=\"References\"></a><H2>REFERENCES</H2>).*", "<b>_____________________</b><br /><br />");
+            html = html.replaceAll("(?s)(\\s+<a id=\"External_links\" name=\"External_links\"></a><H2>EXTERNAL LINKS</H2>).*","<b>_____________________</b><br /><br />");
             html = html.replaceAll("<li class=\"toclevel-1\"><a href=\"#References\">References</a>\n</li>", ""); // JK needs to fix - we think it is ending on a unescaped foreward slash in file title
             html = html.replaceAll("<li class=\"toclevel-1\"><a href=\"#External_links\">External links</a>\n</li>", "");
 //            html = html.replaceAll("<a id=\"See_also\" name=\"See_also\"></a><H2>SEE ALSO</H2>", "");
             html = html.replaceAll("<li class=\"toclevel-1\"><a href=\"#See_also\">See also</a>\n</li>","");
-            //html = html.replaceAll("(\\s+<a id).+?(?:(</a>))","");//removes anchor tags before H2 sections
+            //html = html.replaceAll("(\\s+<a id) (?:(</a>))","");//removes anchor tags before H2 sections
             // text is now html (This is doing iText work)
             convertHtml2Pdf(html);
             // text has been made into pdf.
@@ -495,6 +610,7 @@ public class PdfPageWrapper {
     private WikiFontSelector _wikiFontSelector = null;
     public static FontSelector fs = new FontSelector();
     private FontSelector tfs = new FontSelector();
+    public static FontSelector pfs = new FontSelector();
     private MultiColumnText mct = null;
     private String outputFileName = "";
     private String currentTitle = "";
