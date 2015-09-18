78865a0a2e44ba7465f9e2cb7baba77c48809223
hachacha <jkiritharan@gmail.com>
2015-09-18 16:23:03 -0400

    updated details of pass.py

---
+++ curl/pass.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+lulu_email=""
+lulu_pass = ""
+input_file = "5949&&&Rural Municipality of Terrell No. 101&&&Rush Street (album)&&&.pdf"
+author_fn = ""
+author_ln = ""
+ftp_usr = lulu_email 
+ftp_host = "ftpupload.lulu.com"
+make_log = False
+upload_type = "reg" #contrib/toc/reg
