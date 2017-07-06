8dfb18c67f3430848eb55f0c951c86e0e94a889a
Michael Mandiberg <firstname@lastname.com>
2017-07-06 15:22:51 -0400

    2nd cleanup for leftovers from Oct 9, 2016

--- upload/potentially_useful_utilities/getalltitles.py
+++ upload/potentially_useful_utilities/getalltitles.py
@@ -1,19 +1,51 @@
 import os
 
 
-f = open('alltitles.txt','w')
-
+f = open('repeated_titles.txt','w')
+k = open('all_titles.txt','w')
+repeated_titles = []
 pwd = os.path.abspath(os.getcwd())
 in_folder = pwd + "/../../dist/output/"
 in_items =  os.listdir(in_folder)
 x=0
+hit_counter = 0
+is_matched=False
+
+#print in_items
 while x < len(in_items):
 	item = in_items[x]
+	#print item
 	if item == ".DS_Store" or item[0] == "_":
+		x=x+1
 		continue
+		
 	splitFile = item.split('&&&')
-	# print item
-	f.write("vol: " + splitFile[0] + " " + splitFile[1] + " - " + splitFile[2] + " \n")
 
-	x+=1
+	k.write(splitFile[0] + ", " + splitFile[1] + ", " + splitFile[2] + "\n")
+	# print item
+	# print splitFile[1][0:3] + "     " + splitFile[2][0:3]
+	if splitFile[1][0:3] == splitFile[2][0:3]:
+		if is_matched:
+			hit_counter+=1
+			x+=1 
+			continue
+		is_matched = True
+		d= splitFile[1][0:3]
+		hit_counter+=1
+		print item +  " " + d
+		
+	elif is_matched and d!=splitFile[2][0:3] :	
+		print "in elseif " + d 
+		f.write(d+" " + str(hit_counter)+"\n")
+		is_matched= False
+		hit_counter=0
+		
+
+
+	x=x+1
+
+#d = {y:repeated_titles.count(y) for y in repeated_titles}
+
+
 f.close()
+k.close()
--- upload/potentially_useful_utilities/pagesizepdf.py
+++ upload/potentially_useful_utilities/pagesizepdf.py
@@ -10,7 +10,8 @@ in_items =  os.listdir(in_folder)
 x=1
 while x < len(in_items):
 	reader = pyPdf.PdfFileReader(open(in_folder+in_items[x]))
-	if reader.getNumPages() > 715:
+	if reader.getNumPages() > 714:
+		print in_items[x]
 		f.write(in_items[x] + "\n")
 	x+=1
 
